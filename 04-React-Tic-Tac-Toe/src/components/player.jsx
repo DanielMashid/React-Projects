2e5bd5966faa32bfbcad26a2d9a0d9a1db62d62b
Daniel Mashid <daniel@ametos.com>
2025-01-19 11:12:20 +0200

    1. Added useState & updating state based on old state correctly 2. Conditional content & a suboptimal way of updating state 3. User input & two way-binding

--- 04-React-Tic-Tac-Toe/src/components/player.jsx
+++ 04-React-Tic-Tac-Toe/src/components/player.jsx
@@ -1,11 +1,38 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+	const [playerName, setPlayerName] = useState(initialName);
+	const [isEditing, setIsEditing] = useState(false);
+
+	// Not recommended to use this approach
+	// function handleEditClick() {
+	// 	setIsEditing(!isEditing);
+	// }
+
+	// This is the recommended approach
+	function handleEditClick() {
+		setIsEditing((prevIsEditing) => !prevIsEditing);
+	}
+
+	function handleChange(event) {
+		setPlayerName(event.target.value); // event.target is the input element
+	}
+
+	let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+	if (isEditing) {
+		editablePlayerName = (
+			<input type="text" required value={playerName} onChange={handleChange} />
+		);
+	}
+
 	return (
 		<li>
 			<span className="player">
-				<span className="player-name">{name}</span>
+				{editablePlayerName}
 				<span className="player-symbol">{symbol}</span>
 			</span>
-			<button>Edit</button>
+			<button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
 		</li>
 	);
 }

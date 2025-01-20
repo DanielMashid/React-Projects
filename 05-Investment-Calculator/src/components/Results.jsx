ea88890efa0c889def9275c37d9c6aa78240e41b
Daniel Mashid <daniel@ametos.com>
2025-01-20 13:51:17 +0200

    Outputting results in a list

--- 05-Investment-Calculator/src/components/Results.jsx
+++ 05-Investment-Calculator/src/components/Results.jsx
@@ -1,7 +1,31 @@
+import { calculateInvestmentResults } from "../util/investment.js";
+
+// Year Investment Value Interest (Year) Total Investment Interest Capital
+
 export default function Results({ inputData }) {
+	const resultData = calculateInvestmentResults(inputData); // inputData has the initialInvestment, annualInvestment, expectedReturn, and duration values
 	return (
-		<section id="result">
-			<h2>Results</h2>
-		</section>
+		<table id="result">
+			<thead id="result-thead">
+				<tr>
+					<th>Year</th>
+					<th>Investment Value</th>
+					<th>Interest (Year)</th>
+					<th>Total Investment</th>
+					<th>Interest Capital</th>
+				</tr>
+			</thead>
+			<tbody id="result-tbody">
+				{resultData.map((result, index) => (
+					<tr key={index}>
+						<td>{result.year}</td>
+						<td>{result.valueEndOfYear}</td>
+						<td>{result.interest}</td>
+						<td>{result.annualInvestment}</td>
+						<td>{result.valueEndOfYear - result.annualInvestment}</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
 	);
 }

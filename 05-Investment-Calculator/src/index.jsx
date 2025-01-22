0684aeb21c7dffdec6d4f97540fb163044c8e365
Daniel Mashid <daniel@ametos.com>
2025-01-22 15:42:06 +0200

    Example for StrictMode

--- 05-Investment-Calculator/src/index.jsx
+++ 05-Investment-Calculator/src/index.jsx
@@ -1,6 +1,17 @@
+import { StrictMode } from 'react'; // StrictMode is a tool for highlighting potential problems in an application.
+
 import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+// StrictMode is a react component used to identify the potential issues and bug in the react code
+// Example how to use React.StrictMode
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );

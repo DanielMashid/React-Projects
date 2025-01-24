4c2ac8b166682dd06dad7a9a88fb324cad74e09e
Daniel Mashid <daniel@ametos.com>
2025-01-24 22:30:09 +0200

    Manipulating the DOM via Refs

--- 07-Final-Countdown/src/components/Player.jsx
+++ 07-Final-Countdown/src/components/Player.jsx
@@ -6,6 +6,7 @@ export default function Player() {
 
 	function handleClick() {
 		setEnteredPlayerName(playerName.current.value);
+		playerName.current.value = ''; // Manipulating DOM directly (not recommended)
 	}
 
 	return (

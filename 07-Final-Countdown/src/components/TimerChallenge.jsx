4722c4d3e6bf59218ebd684de65c96ee3cb8c99d
Daniel Mashid <daniel@ametos.com>
2025-01-26 11:38:03 +0200

    - Setting Timers & Managing State - Using let variable is not good solution

--- 07-Final-Countdown/src/components/TimerChallenge.jsx
+++ 07-Final-Countdown/src/components/TimerChallenge.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import { useState } from 'react';
+
+// let timer; We can declare timer here, because it's will be share across all component instances
 
 export default function TimerChallenge({ title, targetTime }) {
+	const [timeStarted, setTimeStarted] = useState(false);
+	const [timerExpired, setTimerExpired] = useState(false);
+
+	// let timer; We can declare timer here, because it's redeclared every time the component is rendered
+
+	function handleStartChallenge() {
+		timer = setTimeout(() => {
+			setTimerExpired(true);
+		}, targetTime * 1000);
+		setTimeStarted(true);
+	}
+
+	function handleStopChallenge() {
+		clearTimeout(timer);
+	}
+
 	return (
 		<section className="challenge">
 			<h2>{title}</h2>
+			{timerExpired && <p> Time's up!</p>}
 			<p className="challenge-time">
 				{targetTime} second{targetTime > 1 ? 's' : ''}
 			</p>
 			<p>
-				<button>Start Challenge</button>
+				<button onClick={timeStarted ? handleStopChallenge : handleStartChallenge}>
+					{timeStarted ? 'Stop' : 'Start'} challenge
+				</button>
 			</p>
-			<p className="challenge-time">Time is running...</p>
+			<p className={timeStarted ? 'active' : undefined}>
+				{timeStarted ? 'Time is running...' : 'Timer inactive'}
+			</p>
 		</section>
 	);
 }

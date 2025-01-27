c61015e79c6fdf2aa4b59fbdc5514b7792469e95
Daniel Mashid <daniel@ametos.com>
2025-01-27 16:42:46 +0200

    Styling Buttons & Inputs with Tailwind CSS

--- 08-Project-Management-App/src/components/Input.jsx
+++ 08-Project-Management-App/src/components/Input.jsx
@@ -1,8 +1,15 @@
 export default function Input({ label, textarea, ...props }) {
+	const classes =
+		'w-full px-4 py-2 border border-stone-500 rounded-md focus:outline-none focus:ring focus:ring-stone-400';
+
 	return (
-		<p>
-			<label>{label}</label>
-			{textarea ? <textarea {...props} /> : <input {...props} />}
+		<p className="flex flex-col gap-1 my-4">
+			<label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+			{textarea ? (
+				<textarea className={classes} {...props} />
+			) : (
+				<input className={classes} {...props} />
+			)}
 		</p>
 	);
 }

d237b07b7310477db756f7b5e2d6b4bb944479e7
Daniel Mashid <daniel@ametos.com>
2025-01-28 19:01:47 +0200

    Adding "Project Tasks" & A Tasks Component

---
+++ 08-Project-Management-App/src/components/NewTask.jsx
@@ -0,0 +1,8 @@
+export default function NewTask() {
+	return (
+		<div>
+			<input type="text" />
+			<button>Add Task</button>
+		</div>
+	);
+}
---
+++ 08-Project-Management-App/src/components/Tasks.jsx
@@ -0,0 +1,12 @@
+import NewTask from './NewTask.jsx';
+
+export default function Tasks() {
+	return (
+		<section>
+			<h2>Tasks</h2>
+			<NewTask />
+			<p>This project does not have any</p>
+			<ul></ul>
+		</section>
+	);
+}

751fe2c2abbe5fb7de09785b743d01c0d76046e9
Daniel Mashid <daniel@ametos.com>
2025-01-29 11:49:42 +0200

    Added style to Tasks

--- 08-Project-Management-App/src/components/Tasks.jsx
+++ 08-Project-Management-App/src/components/Tasks.jsx
@@ -9,11 +9,11 @@ export default function Tasks({ tasks, onAddTaskFromApp, onDeleteTaskFromApp }) 
 				<p className="text-stone-800 my-4">This project does not have any tasks yet</p>
 			)}
 			{tasks.length > 0 && (
-				<ul>
+				<ul className="p-4 mt-8 rounded-md bg-stone-100">
 					{tasks.map((task) => (
-						<li key={task.id}>
+						<li key={task.id} className="flex justify-between my-4">
 							<span>{task.text}</span>
-							<button>Clear</button>
+							<button className="text-stone-700 hover:text-red-500">Clear</button>
 						</li>
 					))}
 				</ul>

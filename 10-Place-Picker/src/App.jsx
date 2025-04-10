49ccab6eacd5a82b6680190d61f2a0d5654aa1cd
Daniel Mashid <daniel@ametos.com>
2025-04-10 21:36:33 +0300

    Using useEffect for Syncing With Browser APIs +ESC key

--- 10-Place-Picker/src/App.jsx
+++ 10-Place-Picker/src/App.jsx
@@ -85,7 +85,7 @@ function App() {
 
 	return (
 		<>
-			<Modal open={modelIsOpen}>
+			<Modal open={modelIsOpen} onClose={handleStopRemovePlace}>
 				<DeleteConfirmation onCancel={handleStopRemovePlace} onConfirm={handleRemovePlace} />
 			</Modal>
 

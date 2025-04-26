778a292e46cb4f55880ff88df8825925499d29f9
Daniel Mashid <daniel@ametos.com>
2025-04-26 11:51:00 +0300

    Preparing the App For Data Fetching

--- 14-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 14-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -1,12 +1,15 @@
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+	// Todo: Fetch available places from backend API
+	const [availablePlaces, setAvailablePlaces] = useState([]);
+
+	return (
+		<Places
+			title="Available Places"
+			places={availablePlaces}
+			fallbackText="No places available."
+			onSelectPlace={onSelectPlace}
+		/>
+	);
 }

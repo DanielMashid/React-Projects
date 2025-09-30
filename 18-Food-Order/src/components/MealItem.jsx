7d1c62f839325c1bbf71eecb62410022af8c6076
Daniel Mashid <daniel@ametos.com>
2025-09-30 12:37:27 +0300

    Adding a "MealItem" Component

---
+++ 18-Food-Order/src/components/MealItem.jsx
@@ -0,0 +1,18 @@
+export default function MealItem({ meal }) {
+	return (
+		<li className="meal-item">
+			<article>
+				{/* port 3000 because that's where the images are served from */}
+				<img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+				<div>
+					<h3>{meal.name}</h3>
+					<p className="meal-item-price">${meal.price}</p>
+					<p className="meal-item-description">{meal.description}</p>
+				</div>
+				<p className="meal-item-actions">
+					<button>Add To Cart</button>
+				</p>
+			</article>
+		</li>
+	);
+}

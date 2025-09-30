21072306b3eee9d753163bb1daa45c813d2f4291
Daniel Mashid <daniel@ametos.com>
2025-09-30 16:05:58 +0300

    Added logic to Add Item action

--- 18-Food-Order/src/store/CartContext.jsx
+++ 18-Food-Order/src/store/CartContext.jsx
@@ -8,9 +8,27 @@ const CartContext = createContext({
 
 function cartReducer(state, action) {
 	if (action.type === 'ADD_ITEM') {
-		// logic for adding item to cart
+		// state.items.push(action.payload); Not good --> never mutate state directly
+
+		// Find the existing item in the cart
+		const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+
+		// Create a new array with the updated items
+		const updatedItems = [...state.items];
+
+		if (existingCartItemIndex > -1) {
+			const existingItem = state.items[existingCartItemIndex];
+			const updatedItem = {
+				...existingItem,
+				quantity: existingItem.quantity + 1,
+			};
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
+			// Item does not exist in cart, add it with quantity 1
+			updatedItems.push({ ...action.item, quantity: 1 });
+		}
+		return { ...state, items: updatedItems };
 	} else if (action.type === 'REMOVE_ITEM') {
-		// logic for removing item from cart
 	}
 	return state;
 }
